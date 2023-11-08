8cc75e8f899ee4c7708d6f0334ded8ef61b41506
22-37621 <22-37621@g.batstate-u.edu.ph>
2023-11-08 12:32:43 +0800

    feat/list-animal-w-most-appoinment

--- veterinary.sql
+++ veterinary.sql
@@ -161,4 +161,13 @@ VALUES
 	WHERE o.ofirstname = 'Maria'
 	GROUP BY o.ofirstname;
 
+    SELECT a.animalid, a.name, COUNT(*) AS appointment_count
+    FROM animals a
+    JOIN appointments ap 
+	ON a.animalid = ap.animalid
+    GROUP BY a.animalid, a.name
+    ORDER BY appointment_count DESC
+    LIMIT 1;
+
+
 
