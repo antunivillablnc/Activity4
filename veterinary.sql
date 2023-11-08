5e42739958ad96d94fd84cdb50bcebcb82dec59a
22-37621 <22-37621@g.batstate-u.edu.ph>
2023-11-08 09:37:05 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -1,11 +1,23 @@
 CREATE DATABASE Veterinary;
 
 CREATE TABLE owners(
-    ownerid PRIMARY KEY,
+    ownerid SERIAL PRIMARY KEY,
     ofirstname VARCHAR(50),
     olastname VARCHAR(50),
     address VARCHAR(100),
     phone VARCHAR(50),
     email VARCHAR(100)
 
+);
+
+CREATE TABLE animals(
+	animalid SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+	ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
 );
