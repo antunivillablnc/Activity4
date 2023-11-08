61106b5e91063bc3e61d498743e8e27291f778f1
Anthony <22-38317@g.batstate-u.edu.ph>
2023-11-08 09:15:15 +0800

    Created owners table

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE Veterinary;
+
+CREATE TABLE owners(
+    ownerid PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(50),
+    email VARCHAR(100)
+
+);
